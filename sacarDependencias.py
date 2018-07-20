7e302921b0ee2f8e95e2404dc1374b1666629d85
bonber <mitxarro1994>
2018-07-20 11:03:11 +0200

    Initial commit

---
+++ sacarDependencias.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+import argparse
+import os.path
+
+#Funciones
+def revisarImport(linea):
+    if 'import ' in linea or ' import ' in linea:
+        if  "from" in linea:
+            linea = linea.split(' ')
+            f.write(linea[3])
+            print('Añadida: ' + linea[3])
+        else:
+            linea = linea.split(' ')
+            f.write(linea[1])
+            print('Añadida: ' + linea[1])
+
+def leerFichero(fichero):
+    with open(fichero, "r") as ObjFichero:
+        for line in ObjFichero:
+            revisarImport(line)
+
+#Argumentos
+parser = argparse.ArgumentParser()
+parser.add_argument("-f" , "--file",  help="Fichero a depurar")
+parser.add_argument("-d" , "--directory",  help="Directorio a depurar")
+args = parser.parse_args()
+
+if not args.file and not args.directory:
+    parser.print_help()
+else:
+    if args.file:
+        f = open("requeriments.txt", 'w')
+        leerFichero(args.file)
+        f.close()
+
+    if args.directory:
+        f = open("requeriments.txt", 'w')
+        for dirpath, dirnames, filenames in os.walk(args.directory):
+            for filename in [f for f in filenames if f.endswith(".py")]:
+                leerFichero(os.path.join(dirpath, filename))
+        f.close()
+
